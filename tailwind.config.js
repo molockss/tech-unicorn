e4391c4a69b92b2ac697cf424d1045eba3cd1fad
molockss <mahamed_67@hotmail.com>
2023-06-10 18:06:29 +0400

    style: add custom font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['DM Sans']
+    },
     extend: {
       colors: {
         primary: '#F86338'
